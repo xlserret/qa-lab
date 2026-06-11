a155ef76af8f3c1f3f96790274d8982523efa457
Xavier Serret <xavier@jserret-ethernet.home>
2026-06-10 17:24:54 -0700

    Add reporter capablities for JUnit

--- automation/playwright/playwright.config.ts
+++ automation/playwright/playwright.config.ts
@@ -22,13 +22,19 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [ 
+    ['html'],
+    ['junit', { outputFile: 'test-results/results.xml' }]
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     trace: 'on-first-retry',
   },
 
